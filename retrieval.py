3de785654273a56751795f1c8dc93adeff8dc81c
porrittmichael <porrittmichael@gmail.com>
2022-01-05 10:06:20 +1100

    first commit

---
+++ retrieval.py
@@ -0,0 +1,20 @@
+# ===WIP===
+# Functions to search and retrieve light curves.
+
+def search_data(star_id):
+    """ Searches for all available data given a systems [?] ID
+    """
+    # search_lightkurve, 
+    # tess-point - to see if theres ffi data
+    # if theres not ffi data use eleanor
+    
+    
+
+def get_data(star_id, mission="all"):
+    """ Compiles all requested data for a given [?] ID
+    """
+    
+class MissionData(data_sets):
+    def __init__(self, data_sets):
+        self.data_sets = data_sets
+        
